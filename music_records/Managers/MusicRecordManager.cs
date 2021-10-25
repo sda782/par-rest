6a1929a1d3208a4c4af3ebaa79660619b1cd7537
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2021-10-25 09:58:43 +0200

    Update MusicRecordManager.cs

--- music_records/Managers/MusicRecordManager.cs
+++ music_records/Managers/MusicRecordManager.cs
@@ -7,9 +7,10 @@ namespace music_records.Managers
 {
     public class MusicRecordManager
     {
-
+        private static int nextId = 0;
         private static readonly List<MusicRecord> manager = new List<MusicRecord>(){
-            new MusicRecord(1, "record 1", "amazing label", 70, DateTime.Now, 10)
+            new MusicRecord(nextId++, "record 1", "amazing label", 70, DateTime.Now, 10),
+            new MusicRecord(nextId++, "record 2", "shit label", 50, DateTime.Now, 15)
         };
 
         public IEnumerable<MusicRecord> Get_Records()
