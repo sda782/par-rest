0fa0c6e7a9add3d314b7b06adc5a9a225ec3d029
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2021-10-25 10:14:48 +0200

    unittest

--- unit_test/UnitTest1.cs
+++ unit_test/UnitTest1.cs
@@ -1,4 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using music_records.Managers;
+using music_records.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace unit_test
 {
@@ -6,8 +11,12 @@ namespace unit_test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetRecord()
         {
+            int expectedResult = 2;
+            MusicRecordManager mrm = new MusicRecordManager();
+            IEnumerable<MusicRecord> records = mrm.Get_Records();
+            Assert.AreEqual(expectedResult, records.Count());
         }
     }
 }
